966056abb42d82ef78e5ebde9e60b0d2b23c4d1a
Jieshun Wang <jieshun.wang@student.unsw.edu.au>
2023-01-25 16:51:31 +1100

    Add existing project files to Git

--- src/index.js
+++ src/index.js
@@ -2,16 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ThemeProvider, createTheme } from '@mui/material';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#f9a859"
+        },
+        background: {
+            default: '#fff'
+        }
+    },
+    typography: {
+        button: {
+            fontSize: "1rem",
+        },
+        logo: {
+            fontFamily: "roboto",
+            fontWeight: 400,
+            fontSize: "2rem",
+            lineHeight: 1,
+            color: "#000000"
+        }
+    }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
